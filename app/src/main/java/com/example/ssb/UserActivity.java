5ef67b2634532f9b59f2afb4ab9cc67a8b955984
pb0c <p.boc89@gmail.com>
2019-12-25 23:54:27 -0300

    verificación usuario ok

--- app/src/main/java/com/example/ssb/UserActivity.java
+++ app/src/main/java/com/example/ssb/UserActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.ssb.dal.UserDAL;
+import com.example.ssb.dto.User;
 import com.example.ssb.helpers.DatabaseHelper;
 
 public class UserActivity extends AppCompatActivity {
@@ -26,6 +28,10 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        userDAL = new UserDAL(getApplicationContext(), new User());
+        final DatabaseHelper helper = new DatabaseHelper(this,"alarma.db",null,1);
+
+
         this.editId = findViewById(R.id.editId);
         this.editPass = findViewById(R.id.editPass);
         this.btnAcceder = findViewById(R.id.btnAcceder);
@@ -36,7 +42,7 @@ public class UserActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Cursor cursor = dbHelper.verif(editId.getText().toString(),editPass.getText().toString());
+                Cursor cursor = helper.verif(editId.getText().toString(),editPass.getText().toString());
 
                 if (cursor.getCount()>0){
                     Intent btnAcceder = new Intent(UserActivity.this, MenuActivity.class);
@@ -44,6 +50,7 @@ public class UserActivity extends AppCompatActivity {
                     startActivity(btnAcceder);
                 }else {
                     Toast.makeText(getApplicationContext(),"Usuario o Contraseña incorrecto",Toast.LENGTH_LONG).show();
+
                 }
                 editId.setText("");
                 editPass.setText("");
